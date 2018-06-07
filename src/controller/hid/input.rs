95a7003fd0f79ca7b78b1e90d5c88901cfaaf427
Alexander Peters <alexander.n.peters@gmail.com>
2018-06-07 02:13:11 -0700

    Map input report codes to InputReport types

--- src/controller/hid/input.rs
+++ src/controller/hid/input.rs
@@ -1,3 +1,21 @@
+use self::InputReport::*;
+
 pub enum InputReport {
-    Standard,
+    CommandResponse,
+    FullInput,
+    ExtendedInput,
+    SimpleInput,
+    Unknown,
+}
+
+impl From<u8> for InputReport {
+    fn from(code: u8) -> InputReport {
+        match code {
+            0x21 => CommandResponse,
+            0x30 | 0x32 | 0x33 => FullInput,
+            0x31 => ExtendedInput,
+            0x3f => SimpleInput,
+            _ => Unknown,
+        }
+    }
 }
