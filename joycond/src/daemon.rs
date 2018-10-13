b4544455d495988f7f02405a5f6bd5a96eec7051
Alexander Peters <alexander.n.peters@gmail.com>
2018-10-13 15:10:10 -0700

    Set up basic joycond entrypoint

---
+++ joycond/src/daemon.rs
@@ -0,0 +1,36 @@
+use std::io::Error;
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::thread;
+
+use common::ipc::paths;
+use common::log;
+use common::types::Never;
+
+//TODO This should really be Result<!, Error>, watch `never_type` feature progress
+pub fn listen() -> Result<Never, Error> {
+    let listener = match UnixListener::bind(paths::DAEMON_PATH) {
+        Ok(l) => l,
+        Err(e) => panic!("Couldn't open socket: {:?}", e),
+    };
+
+    for stream in listener.incoming() {
+        let stream = match stream {
+            Ok(s) => s,
+            Err(e) => {
+                log::e(&format!("Failed to connect: {:?}", e));
+                continue;
+            }
+        };
+
+        thread::spawn(move || {
+            handle_connection(stream);
+        });
+    }
+
+    //FIXME Use a real Error::new() here
+    Err(Error::last_os_error())
+}
+
+fn handle_connection(stream: UnixStream) {
+    // Do things here
+}
--- joycond/src/main.rs
+++ joycond/src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+extern crate common;
+
+mod daemon;
+
+fn main() {
+    match daemon::listen() {
+        Err(e) => panic!(e),
+        _ => (),
+    }
+}
