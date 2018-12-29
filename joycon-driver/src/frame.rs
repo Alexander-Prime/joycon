136cb0ce125f0db1c4a10572f029f9967b643a9e
Alexander Peters <alexander.n.peters@gmail.com>
2018-12-29 13:09:13 -0700

    Change ButtonFrame to a simple tuple struct

--- joycon-driver/src/frame.rs
+++ joycon-driver/src/frame.rs
@@ -3,7 +3,7 @@ use byteorder::{ByteOrder, LittleEndian};
 use common::has::Has;
 
 use super::axis::Axis;
-use super::button::{Button, Button::*};
+use super::button::Button;
 
 pub struct InputFrame {
     buttons: ButtonFrame,
@@ -14,130 +14,27 @@ pub struct InputFrame {
 impl InputFrame {
     pub fn new() -> InputFrame {
         InputFrame {
-            buttons: ButtonFrame::new(),
+            buttons: Default::default(),
             axes: AxisFrame::new(),
             motion: MotionFrame::new(),
         }
     }
 }
 
-pub struct ButtonFrame {
-    y: bool,
-    x: bool,
-    b: bool,
-    a: bool,
-    right_sr: bool,
-    right_sl: bool,
-    r: bool,
-    zr: bool,
-
-    minus: bool,
-    plus: bool,
-    cr: bool,
-    cl: bool,
-    home: bool,
-    capture: bool,
-
-    down: bool,
-    up: bool,
-    right: bool,
-    left: bool,
-    left_sr: bool,
-    left_sl: bool,
-    l: bool,
-    zl: bool,
-}
+#[derive(Default)]
+pub struct ButtonFrame([u8; 3]);
 
 impl<'a> From<&'a [u8]> for ButtonFrame {
     fn from(buf: &[u8]) -> ButtonFrame {
-        ButtonFrame {
-            y: buf.has(Y),
-            x: buf.has(X),
-            b: buf.has(B),
-            a: buf.has(A),
-            right_sr: buf.has(RightSr),
-            right_sl: buf.has(RightSl),
-            r: buf.has(R),
-            zr: buf.has(Zr),
-
-            minus: buf.has(Minus),
-            plus: buf.has(Plus),
-            cr: buf.has(Cr),
-            cl: buf.has(Cl),
-            home: buf.has(Home),
-            capture: buf.has(Capture),
-
-            down: buf.has(Down),
-            up: buf.has(Up),
-            right: buf.has(Right),
-            left: buf.has(Left),
-            left_sr: buf.has(LeftSr),
-            left_sl: buf.has(LeftSl),
-            l: buf.has(L),
-            zl: buf.has(Zl),
-        }
-    }
-}
-
-impl ButtonFrame {
-    pub fn new() -> ButtonFrame {
-        ButtonFrame {
-            y: false,
-            x: false,
-            b: false,
-            a: false,
-            right_sr: false,
-            right_sl: false,
-            r: false,
-            zr: false,
-
-            minus: false,
-            plus: false,
-            cr: false,
-            cl: false,
-            home: false,
-            capture: false,
-
-            down: false,
-            up: false,
-            right: false,
-            left: false,
-            left_sr: false,
-            left_sl: false,
-            l: false,
-            zl: false,
-        }
+        let mut buttons: [u8; 3] = Default::default();
+        buttons.copy_from_slice(&buf);
+        ButtonFrame(buttons)
     }
 }
 
 impl<'a> Has<Button> for ButtonFrame {
     fn has(&self, btn: Button) -> bool {
-        match btn {
-            Y => self.y,
-            X => self.x,
-            B => self.b,
-            A => self.a,
-            RightSr => self.right_sr,
-            RightSl => self.right_sl,
-            R => self.r,
-            Zr => self.zr,
-
-            Minus => self.minus,
-            Plus => self.plus,
-            Cr => self.cr,
-            Cl => self.cl,
-            Home => self.home,
-            Capture => self.capture,
-
-            Down => self.down,
-            Up => self.up,
-            Right => self.right,
-            Left => self.left,
-            LeftSr => self.left_sr,
-            LeftSl => self.left_sl,
-            L => self.l,
-            Zl => self.zl,
-        }
+        (&self.0[..]).has(btn)
     }
 }
 
